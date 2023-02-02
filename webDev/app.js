8e389d2a5e867cafa0fa31f68222cbd032360b91
Eitan Cohen <eitancohen77@gmail.com>
2023-02-01 23:38:45 -0500

    Add game physics

--- webDev/app.js
+++ webDev/app.js
@@ -7,17 +7,29 @@ $(document).ready(function() {
     for (let i = 0; i < circles.length; i++) {
         let circle = circles[i];
         circle.setAttribute('id', count);
+        circle.setAttribute('taken', false);
         count++;
 
         circle.addEventListener('click', function() {
-            if (parseInt(circle.getAttribute('id')) >= 0 && counter == -1) {
+            let id = parseInt(circle.getAttribute('id')) % 7
+            let topCircle = document.getElementById(id);
+            console.log(topCircle.style.backgroundColor + ' tpp id number is ' + topCircle.getAttribute('id'))
+            while (id < 42 && topCircle.getAttribute('taken') === 'false') {
+                console.log(topCircle);
+                circle = topCircle;
+                id += 7
+                topCircle = document.getElementById(id);
+            }
+            if ((circle.getAttribute('taken')) === 'false' && counter == -1) {
                 circle.style.backgroundColor = 'red';
+                circle.setAttribute('taken', true);
                 counter *= -1;
             }
-            else if (parseInt(circle.getAttribute('id')) >= 0 && counter == 1) {
+            else if ((circle.getAttribute('taken')) === 'false' && counter == 1) {
                 circle.style.backgroundColor = 'yellow';
+                circle.setAttribute('taken', true);
                 counter *= -1;
-            }
+            } 
         });
     }
 });
